e126dd88299ea52067e6077061d9fd3cc08aed76
Matias Verschae <matias.verschae.herrera@gmail.com>
2026-04-11 21:01:36 -0400

    ultimos Cambios

--- extra.c
+++ extra.c
@@ -98,6 +98,5 @@ void limpiarPantalla() { system("clear"); }
 
 void presioneTeclaParaContinuar() {
   puts("Presione una tecla para continuar...");
-  //getchar(); // Consume el '\n' del buffer de entrada
   getchar(); // Espera a que el usuario presione una tecla
 }
